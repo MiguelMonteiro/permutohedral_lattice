1e32997c0006b3259bb78f769b2500b0be798997
Miguel Monteiro <mab.mtr@gmail.com>
2018-01-31 16:50:55 +0000

    CPU kernel working

--- op_test.py
+++ op_test.py
@@ -11,6 +11,7 @@ tf_reference_image = tf.constant(np.array(im)/255.0, dtype=tf.float32)
 
 output = module.bilateral(tf_input_image, tf_reference_image)
 with tf.Session() as sess:
-    o = sess.run(output)
-
-print(o)
+    o = sess.run(output) * 255
+o = np.round(o).astype(np.uint8)
+im = Image.fromarray(o)
+im.save("this_is_it.bmp")
