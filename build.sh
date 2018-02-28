2fcc3ded675bfebe68d3bfad74f1bc0760ecf321
MiguelMonteiro <mab.mtr@gmail.com>
2018-02-28 11:28:42 +0000

    GPU filter params given during build

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+mkdir build_dir
+cd build_dir
+rm -r *
+
+CUDA_COMPILER=/usr/local/cuda/bin/nvcc
+CXX_COMPILER=/usr/bin/g++-4.8
+
+SPATIAL_DIMS=2
+INPUT_CHANNELS=3
+REFERENCE_CHANNELS=3
+
+cmake -DCMAKE_BUILD_TYPE=Debug -D CMAKE_CUDA_COMPILER=${CUDA_COMPILER} \
+                               -D CMAKE_CXX_COMPILER=${CXX_COMPILER} \
+                               -D CMAKE_CUDA_HOST_COMPILER=${CXX_COMPILER} \
+                               -D SPATIAL_DIMS=${SPATIAL_DIMS} \
+                               -D INPUT_CHANNELS=${INPUT_CHANNELS} \
+                               -D REFERENCE_CHANNELS=${REFERENCE_CHANNELS} \
+                               -G "CodeBlocks - Unix Makefiles" ../
+
+
+make
--- remote_build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-CUDA_COMPILER=/usr/local/cuda/bin/nvcc
-CXX_COMPILER=/usr/bin/g++-4.8
-
-cmake -DCMAKE_BUILD_TYPE=Debug -D CMAKE_CUDA_COMPILER=${CUDA_COMPILER} -D CMAKE_CXX_COMPILER=${CXX_COMPILER} -D CMAKE_CUDA_HOST_COMPILER=${CXX_COMPILER} -G "CodeBlocks - Unix Makefiles" ~/permutohedral_lattice/
