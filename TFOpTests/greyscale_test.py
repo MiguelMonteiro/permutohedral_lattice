ce697481c88dd4b631d807792dd022317237ebc3
MiguelMonteiro <mab.mtr@gmail.com>
2018-03-09 17:03:52 +0000

    refactor

--- TFOpTests/greyscale_test.py
+++ TFOpTests/greyscale_test.py
@@ -28,7 +28,7 @@ with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
     o = np.round(sess.run(output) * 255).astype(np.uint8)
 
 im = Image.fromarray(grey)
-im.save('TFOPTests/Results/gray_original.bmp')
+im.save('TFOpTests/Results/gray_original.bmp')
 
 im = Image.fromarray(np.squeeze(o[0]))
-im.save('TFOPTests/Results/filtered_grey.bmp')
+im.save('TFOpTests/Results/filtered_grey.bmp')
